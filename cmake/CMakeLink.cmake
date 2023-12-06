2af4655ae5da1036da196aefa3d1bfd7fb80c21d
Ruben Laso <laso@par.tuwien.ac.at>
2023-12-06 17:56:41 +0100

    build: update CMake files

--- cmake/CMakeLink.cmake
+++
@@ -1,9 +0,0 @@
-if (TARGET range-v3::range-v3)
-    target_link_libraries(${PROJECT_NAME} INTERFACE range-v3::range-v3)
-endif ()
-
-if (TARGET fmt::fmt)
-    target_link_libraries(${PROJECT_NAME} INTERFACE fmt::fmt)
-endif ()
-
-target_link_libraries(${PROJECT_NAME} INTERFACE numa)
